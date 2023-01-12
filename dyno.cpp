fa6bce8f7a0cdfb4ab27e1e240428b99eaa919e6
Vasiliy Simonov <simonov.vasiliy@gmail.com>
2023-01-12 22:19:14 +0000

    ESC and LSD working

--- dyno.cpp
+++ dyno.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
+#include "pico/mutex.h"
+#include "pico/multicore.h"
 #include "hardware/i2c.h"
+#include "hardware/pwm.h"
 #include "pico-ssd1306/ssd1306.h"
 #include "pico-ssd1306/textRenderer/TextRenderer.h"
 
@@ -35,10 +38,11 @@ void i2c_scan_address() {
         // Skip over any reserved addresses.
         int ret;
         uint8_t rxdata;
-        if (reserved_addr(addr))
+        if (reserved_addr(addr)) {
             ret = PICO_ERROR_GENERIC;
-        else
+        } else {
             ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+        }
 
         printf(ret < 0 ? "." : "@");
         printf(addr % 16 == 15 ? "\n" : "  ");
@@ -48,6 +52,147 @@ void i2c_scan_address() {
 
 using namespace pico_ssd1306;
 
+struct {
+    const uint throttle = 16;
+    const uint reverse = 15;
+
+    void init() {
+        gpio_pull_up(throttle);
+        gpio_set_dir(throttle, GPIO_IN);
+        gpio_pull_up(reverse);
+        gpio_set_dir(reverse, GPIO_IN);
+    }
+} buttons;
+
+struct Sensor {
+    uint pin;
+    int8_t head;
+    int8_t tail;
+    bool full;
+    uint64_t measuresNs[16];
+    mutex_t mutex;
+
+    Sensor(uint _pin) :
+        pin(_pin),
+        full(false),
+        head(),
+        tail()
+    {
+        gpio_init(pin);
+        gpio_set_dir(pin, GPIO_IN);
+        mutex_init(&mutex);
+    }
+
+    void clear() {
+        mutex_enter_blocking(&mutex);
+        head = 0;
+        tail = 0;
+        full = false;
+        mutex_exit(&mutex);
+    }
+
+    void push(uint64_t measure) {
+        mutex_enter_blocking(&mutex);
+        measuresNs[tail] = measure;
+        if (full) { // overwrite
+            tail = next(tail);
+            head = tail;
+        } else {
+            tail = next(tail);
+            full = (tail == head);
+        }
+        mutex_exit(&mutex);
+    }
+
+    bool pop(uint64_t &prev, uint64_t &curr) { 
+        mutex_enter_blocking(&mutex);
+        bool result = false;
+        if (size() > 1) { // TODO simplify count
+            auto nextHead = next(head);
+            prev = measuresNs[head];
+            curr = measuresNs[nextHead];
+            head = nextHead;
+            full = false;
+            result = true;
+        }
+        mutex_exit(&mutex);
+        return result;
+    }
+
+    int8_t size() {
+        if (full) return 16;
+        auto diff = tail - head;
+        return (tail >= head) ? diff : diff + 16;
+    }
+
+    inline int8_t next(int8_t i) {
+        return (i + 1) & 0xF;
+    }
+};
+
+const uint pinSensorB = 20;
+auto sensorB = Sensor(pinSensorB);
+
+struct {
+    const uint pinEsc = 3;
+    uint slice;
+    uint channel;
+    uint16_t wrap;
+
+    void init() {
+        gpio_set_function(pinEsc, GPIO_FUNC_PWM);
+        slice = pwm_gpio_to_slice_num(pinEsc);
+        channel = pwm_gpio_to_channel(pinEsc);
+        // servo cycle is 20000mks or 50Hz 
+        uint32_t freq = 50;
+        uint32_t clock = 125000000;
+        uint32_t divider16 = clock / (freq * 4096) +
+                            (clock % (freq * 4096) != 0);
+        if (divider16 / 16 == 0) divider16 = 16;
+        wrap = clock * 16 / divider16 / freq - 1;
+        pwm_set_clkdiv_int_frac(slice, divider16 / 16, divider16 & 0xF);
+        pwm_set_wrap(slice, wrap);
+        pwm_set_enabled(slice, true);
+    }
+
+    // duty 1000mks - low
+    // duty 1500mks - mid
+    // duty 2000mks - high
+    void setMicros(uint32_t micros) {
+        pwm_set_chan_level(slice, channel, wrap * micros / 20000); // given 50Hz or 20_000mks cycle
+    }
+} servo;
+
+void measure_spool_up(SSD1306 &lcd) {
+    sensorB.clear();
+    servo.setMicros(2000);
+    auto startNs = time_us_64();
+    while (!gpio_get(buttons.throttle)) {
+        uint64_t prev;
+        uint64_t curr;
+        while (sensorB.pop(prev, curr)) {
+            auto time = curr - startNs;
+            printf("%i\n", time);
+            if (time > 3000000000L) { // sec
+                printf("final rpm %i\n", 6000000000L / (curr - prev));
+                return;
+            }
+        }
+        sleep_ms(50);
+    }
+}
+
+void handle_interrupt(uint gpio, uint32_t event_mask) {
+    if (gpio == pinSensorB) {
+        sensorB.push(time_us_64());
+    }
+}
+
+void setup_sensor_interrupts() {
+    gpio_set_irq_callback(handle_interrupt);
+    gpio_set_irq_enabled(pinSensorB, GPIO_IRQ_EDGE_FALL, true);
+}
+
 int main() {
     stdio_init_all();
 
@@ -55,37 +200,52 @@ int main() {
     gpio_init(led_pin);
     gpio_set_dir(led_pin, GPIO_OUT);
 
-    i2c_setup(i2c0, 20, 21);
-    bi_decl(bi_2pins_with_func(20, 21, GPIO_FUNC_I2C));
-    // display address 0x3c (60)
+    // ads1115 ADC address 0x48 (72)
+    
+    // init display
+    i2c_setup(i2c0, 12, 13);
+    sleep_ms(250);
+    SSD1306 lcd = SSD1306(i2c0, 0x3C, Size::W128xH32);
+    lcd.setOrientation(0);
+    
+    buttons.init();
+    servo.init();
+    multicore_launch_core1(setup_sensor_interrupts);
+
     // ads1115 ADC address 0x48 (72)
 
-    // If you don't do anything before initializing a display pi pico is too fast and starts sending
-    // commands before the screen controller had time to set itself up, so we add an artificial delay for
-    // ssd1306 to set itself up
-    sleep_ms(250);
+    bool lastThrottle = false;
+    for (int i = 0; true; i++) { 
+        // process buttons
+        bool throttle = !gpio_get(buttons.throttle);
+        bool reverse = !gpio_get(buttons.reverse);
 
-    // Create a new display object at address 0x3D and size of 128x32
-    SSD1306 display = SSD1306(i2c0, 0x3C, Size::W128xH32);
+        if (!lastThrottle && throttle) {
+            measure_spool_up(lcd);
+            servo.setMicros(1500);
+        } else if (reverse) {
+            servo.setMicros(1000);
+        } else {
+            servo.setMicros(1500);
+        }
+        lastThrottle = throttle;
 
-    // Here we rotate the display by 180 degrees, so that it's not upside down from my perspective
-    // If your screen is upside down try setting it to 1 or 0
-    display.setOrientation(0);
-
-    // Loop forever
-    for (int i = 0; true; i++) {
+        lcd.clear();
         char line[17];
-        sniprintf(line, 16, "3.0s   %5irpm", i);
-        display.clear();
-        drawText(&display, font_8x8, "8.23V     150.1A", 0 ,0);
-        drawText(&display, font_8x8, line, 0 ,12);
-        display.sendBuffer();
+        sniprintf(line, 16, "3sec          %c%c", reverse ? 'B' : '-', throttle ? 'T' : '-');
+        drawText(&lcd, font_8x8, line, 0, 12);
+        //drawText(&display.lcd, font_8x8, "8.23V     150.1A", 0, 0);
+        lcd.sendBuffer();
+        
+        
+        //i2c_scan_address();
 
-        printf("Blinking!\n");
+        /*
         gpio_put(led_pin, true);
-        sleep_ms(1000);
+        sleep_ms(500);
         gpio_put(led_pin, false);
-        sleep_ms(1000);
-        i2c_scan_address();
+        sleep_ms(500);
+        */
+       sleep_ms(100);
     }
 }
